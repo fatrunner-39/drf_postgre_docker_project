a3d6ccfd060bada5a3947d26adcafb31e354040c
aleksandr <a.chekhonin@code-construction.ru>
2022-12-25 22:09:03 +0200

    fix ren.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "run makemigrations"
 python3 manage.py makemigrations
 sleep 5
 echo "run migrate"
-web python3 manage.py migrate
+python3 manage.py migrate
 
 echo "start work"
 python manage.py runserver 0.0.0.0:8000
